ede467c33bde2becc5259faaf7009cc9d8d6b6a0
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2023-01-28 20:28:19 +0100

    Empezamos a crear el formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,23 @@
 const Formulario = () => {
   return (
-    <div className="md:w-1/2 lg:w-2/5">Formulario</div>
+    <div className="md:w-1/2 lg:w-2/5">
+     <h2 className="font-black text-3xl text-center">Seguimeinto de pacientes</h2> 
+     <p className="text-lg mt-5 text-center mb-10">
+      Añade Pacientes y {' '}
+      <span className=" text-indigo-600 font-bold">Adeministralos</span>
+     </p>
+     <form className=" bg-white shadow-md rounded-lg py-10 px-5">
+      <div>
+        <label htmlFor="mascota" className=" block text-gray-700 font-bold uppercase">Nombre de mascota</label>
+        <input 
+          id="mascota"
+          type="text"
+          placeholder="Nombre de la mascota"
+          className=" border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md "
+        />
+      </div>
+     </form>
+    </div>
   )
 }
 
