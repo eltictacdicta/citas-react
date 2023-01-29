0eb8856a1a419efbae06905440e32f7a6884be95
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2023-01-29 18:48:18 +0100

    Seguimos creando el formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -7,7 +7,7 @@ const Formulario = () => {
       <span className=" text-indigo-600 font-bold">Adeministralos</span>
      </p>
      <form className=" bg-white shadow-md rounded-lg py-10 px-5">
-      <div>
+      <div className="mb-5">
         <label htmlFor="mascota" className=" block text-gray-700 font-bold uppercase">Nombre de mascota</label>
         <input 
           id="mascota"
@@ -15,6 +15,43 @@ const Formulario = () => {
           placeholder="Nombre de la mascota"
           className=" border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md "
         />
+      </div>
+      <div className="mb-5">
+        <label htmlFor="propietario" className=" block text-gray-700 font-bold uppercase">Nombre del propietario</label>
+        <input 
+          id="propietario"
+          type="text"
+          placeholder="Nombre del propietario"
+          className=" border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md "
+        />
+      </div>
+      <div className="mb-5">
+        <label htmlFor="email" className=" block text-gray-700 font-bold uppercase">Email</label>
+        <input 
+          id="email"
+          type="email"
+          placeholder="Email de contacto del propietario"
+          className=" border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md "
+        />
+      </div>
+      <div className="mb-5">
+        <label htmlFor="alta" className=" block text-gray-700 font-bold uppercase">Alta</label>
+        <input 
+          id="alta"
+          type="date"
+          className=" border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md "
+        />
+      </div>
+      <div className="mb-5">
+        <label htmlFor="sintomas" className=" block text-gray-700 font-bold uppercase">Sintomas</label>
+        <textarea 
+        name="sintomas" 
+        id="sintomas" 
+        className=" border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md "
+        placeholder="Describe los sintomas"
+        >
+          
+        </textarea>
       </div>
      </form>
     </div>
